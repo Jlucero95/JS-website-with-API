da10e7b26827660e7a7dcf81d7eea87764de6aad
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-04-11 02:18:25 -0500

    Added styled cards
    Cards now display Name, Desc, Type, Atk, and Def. CSS is very
    basic might need to update it later on.

--- main.js
+++ main.js
@@ -2,4 +2,26 @@ const yuGiOhCards = fetch(
 	"https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&attribute=dark"
 );
 
-yuGiOhCards.then((data) => data.json()).then((data) => console.log(data));
+yuGiOhCards
+	.then((data) => data.json())
+	.then((data) => {
+		function makeCard(nameOf) {
+			let cardArr = Object.values(nameOf);
+			[dataOf] = cardArr;
+			console.log(dataOf);
+			const cardsMarkup = dataOf.map((card) => {
+				return `
+          <div class="card">
+            <h2>${card.name}</h2>
+            <p>Description: ${card.desc}</p>
+            <p>Type: ${card.type}</p>
+            <p>Attack: ${card.atk} <br> Defense: ${card.def}</p>
+          </div>
+        `;
+			});
+			return cardsMarkup.join(""); // join the array of markup strings into a single string
+		}
+		const allCardsContainer = document.getElementById("allCards");
+		allCardsContainer.insertAdjacentHTML("beforeend", makeCard(data));
+	})
+	.catch((err) => console.log(err));
